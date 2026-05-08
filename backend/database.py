a57dd98edfe2f2c22f9d1ce2db83fafcc920f3cb
Asif Ahmed Sahil <asifahmedsahil.12@gmail.com>
2026-05-08 23:45:31 +0600

    declarative_base

---
+++ backend/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+DATABASE = 'todo.py'
+
+SQL_DATABASE_URL = f"sqlite:///{DATABASE}"
+
+engine = create_engine(
+    SQL_DATABASE_URL,
+    connect_args={"check_same_thread":False}
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autoCommit=False
+)
+
+Base = declarative_base()
